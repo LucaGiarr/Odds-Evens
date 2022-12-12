a5b8d8e56afe3a907bb95d1cac0859f792278d7d
LucaGiarr <giarrusso.luca8@gmail.com>
2022-12-12 13:57:01 +0000

    add event listeners forand function fodds and evens buttons to be highlighted when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,9 @@ let maxScoreNumb = "0";
 let howToPlayButton = document.getElementById("how-to-play-button");
 let startButton = document.getElementById("start-button");
 
+let oddsButton = document.getElementById("odds-button");
+let evensButton = document.getElementById("evens-button");
+
 
 // Event listeners
 document.addEventListener("DOMContentLoaded", function () {
@@ -37,6 +40,37 @@ startButton.addEventListener("click", function () {
   }
 })
 
+oddsButton.addEventListener("click", function () {
+
+  if (oddsClicked === "false" && evensClicked === "false") {
+    oddsClicked = "true";
+
+    highlightButton(oddsButton, oddsClicked);
+
+  } else if (oddsClicked === "false" && evensClicked === "true") {
+    oddsClicked = "true";
+    evensClicked = "false";
+    highlightButton(oddsButton, oddsClicked);
+    highlightButton(evensButton, evensClicked);
+  }
+  youChoice = "odd";
+});
+
+evensButton.addEventListener("click", function () {
+
+  if (evensClicked === "false" && oddsClicked === "false") {
+    evensClicked = "true";
+    highlightButton(evensButton, evensClicked);
+
+  } else if (evensClicked === "false" && oddsClicked === "true") {
+    evensClicked = "true";
+    oddsClicked = "false";
+    highlightButton(evensButton, evensClicked);
+    highlightButton(oddsButton, oddsClicked);
+  }
+  youChoice = "even";
+});
+
 
 
 
@@ -44,4 +78,23 @@ startButton.addEventListener("click", function () {
 function displayMaxScoreNumb() {
   document.getElementById("pc-score").innerText = `SCORE (0/${maxScoreNumb})`;
   document.getElementById("you-score").innerText = `SCORE (0/${maxScoreNumb})`;
+}
+
+/**
+ * Highlight the button if clicked and go to the standard button style if the other button is clicked
+ */
+function highlightButton(button, isClicked) {
+
+  let backColor = "black";
+  let textColor = "white";
+  let defaultBackColor = "#EFEFEF";
+  let defaultTextColor = "black";
+
+  if (isClicked === "true") {
+    button.style.backgroundColor = backColor;
+    button.style.color = textColor;
+  } else {
+    button.style.backgroundColor = defaultBackColor;
+    button.style.color = defaultTextColor;
+  }
 }
